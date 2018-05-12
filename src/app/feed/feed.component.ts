540005302394575035b4aa756127c53eca7b3cba
Akhil Vij <akhilvij@gmail.com>
2018-05-12 15:09:29 +0530

    Fetch data from firebase and show in feed component, pass message as Input binding to message component from feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { Observable } from 'rxjs/Observable';
+import { ChatMessage } from '../models/chat-message.model';
+import { AngularFireList } from 'angularfire2/database';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  // Note - Feed is to type AngularFireList
+  feed: Observable<ChatMessage[]>;
+
+  constructor(private chat: ChatService) { }
 
   ngOnInit() {
+    this.feed = this.chat.getMessages().valueChanges();
   }
 
+  ngOnChanges() {
+    this.feed = this.chat.getMessages().valueChanges();
+  }
 }
